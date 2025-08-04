ab4e41d38f541e9f2ee44a4d944c061ec8aeeee1
mkhanz2 <maaz025khanz@gmail.com>
2025-08-04 19:16:01 +0530

    Connected to mongoDB Atlas

--- Db/appDb.js
+++ Db/appDb.js
@@ -1,7 +1,10 @@
 const { name } = require('ejs')
 const mongoose= require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/tastyBytee")
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 const userSchema= mongoose.Schema({
     username: String,
