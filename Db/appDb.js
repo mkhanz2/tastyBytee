e29498bd486bf8ba87e4290d23be558543e1ffac
mkhanz2 <maaz025khanz@gmail.com>
2025-08-04 21:20:46 +0530

    ssl issue

--- Db/appDb.js
+++ Db/appDb.js
@@ -4,7 +4,11 @@ const mongoose= require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  tlsAllowInvalidCertificates: false
 })
 
 const userSchema= mongoose.Schema({
