1f553fbdc76bca577677dcaa4618a95af8d73ded
mkhanz2 <maaz025khanz@gmail.com>
2025-12-07 02:53:04 +0530

    Updated mongoose connection code and removed deprecated options

--- Db/appDb.js
+++ Db/appDb.js
@@ -1,24 +1,18 @@
-require('dotenv').config(); 
-const { name } = require('ejs')
-const mongoose= require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true,
-  tlsAllowInvalidCertificates: false
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("DB Error:", err));
 
-const userSchema= mongoose.Schema({
-    username: String,
-    name:String,
-    email:String,
-    password: String,
-    address: String,
-    number: Number,
-    dob:Date
-})
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  address: String,
+  number: Number,
+  dob: Date
+});
 
-module.exports= mongoose.model("user", userSchema)
+module.exports = mongoose.model("user", userSchema);
